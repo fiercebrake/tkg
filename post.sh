bd0d70fa737d4878e632174197867fd1b5be8b95
David Valiente <fierce.brake@gmail.com>
2025-01-23 15:46:40 -0600

    Update post.sh

--- post.sh
+++ post.sh
@@ -4,11 +4,11 @@ makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --dir /mnt/tkg/ntl
 makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --dir /mnt/tkg/nvidia-all
 makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --dir /mnt/tkg/wine-tkg-git/wine-tkg-git
 
-mv ../ntl/*.pkg.tar.zst ./
-mv ../nvidia-all/*.pkg.tar.zst ./
-mv ../wine-tkg-git/wine-tkg-git/*.pkg.tar.zst ./
+mv /mnt/tkg/ntl/*.pkg.tar.zst /mnt/tkg/repo
+mv /mnt/tkg/nvidia-all/*.pkg.tar.zst /mnt/tkg/repo
+mv /mnt/tkg/wine-tkg-git/wine-tkg-git/*.pkg.tar.zst /mnt/tkg/repo
 
-repo-add -n -v ./themis.db.tar.gz ./*.pkg.tar.zst
+repo-add -n -v /mnt/tkg/repo/themis.db.tar.gz /mnt/tkg/repo/*.pkg.tar.zst
 # scp -P 45100 ./tekne* ./*.pkg.tar.zst devops@sivaractors.com:x86_64/
 # ssh -p 45100 devops@sivaractors.com "sudo chown -R devops:nginx /home/devops/x86_64/"
 # update
