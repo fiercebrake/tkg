e40c1d27c6a0d5cb01fc25cf095886a4b11bd5a6
David Valiente <fierce.brake@gmail.com>
2025-01-23 15:44:21 -0600

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
+makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --dir /mnt/tkg/ntl
+makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --dir /mnt/tkg/nvidia-all
+makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --dir /mnt/tkg/wine-tkg-git/wine-tkg-git
+
 mv ../amd/*.pkg.tar.zst ./
 mv ../ntl/*.pkg.tar.zst ./
 mv ../nvidia-all/*.pkg.tar.zst ./
