42e95f50bfd5dba8b779447efa44d0073708d87d
David Valiente <fierce.brake@gmail.com>
2025-04-08 09:12:23 -0400

    Update post.sh

--- post.sh
+++ post.sh
@@ -8,6 +8,7 @@ mv /mnt/tkg/ntl/*.pkg.tar.zst /mnt/tkg/repo
 mv /mnt/tkg/nvidia-all/*.pkg.tar.zst /mnt/tkg/repo
 mv /mnt/tkg/wine-tkg-git/wine-tkg-git/*.pkg.tar.zst /mnt/tkg/repo
 
+rm -rf /mnt/tkg/repo/themis*
 repo-add -n -v /mnt/tkg/repo/themis.db.tar.gz /mnt/tkg/repo/*.pkg.tar.zst
 # scp -P 45100 ./tekne* ./*.pkg.tar.zst devops@sivaractors.com:x86_64/
 # ssh -p 45100 devops@sivaractors.com "sudo chown -R devops:nginx /home/devops/x86_64/"
