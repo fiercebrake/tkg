11495e6e6bd24cddeb8d2948ae0535ef6e8a7b06
David Valiente <fierce.brake@gmail.com>
2025-09-09 11:17:15 -0600

    Update linuxtkg.sh

--- linuxtkg.sh
+++ linuxtkg.sh
@@ -4,8 +4,26 @@
 # sudo mount -o remount,size=30G,noatime /tmp
 
 git clone https://github.com/Frogging-Family/linux-tkg.git
+git clone https://aur.archlinux.org/onedrive-abraunegg.git
+git clone https://aur.archlinux.org/google-chrome.git
+git clone https://aur.archlinux.org/microsoft-edge-stable-bin.git
+git clone https://aur.archlinux.org/blesh-git.git
+git clone https://aur.archlinux.org/ocs-url.git
+git clone https://aur.archlinux.org/aic94xx-firmware.git
+git clone https://aur.archlinux.org/ast-firmware.git
+git clone https://aur.archlinux.org/wd719x-firmware.git
+git clone https://aur.archlinux.org/upd72020x-fw.git
+git clone https://aur.archlinux.org/laptop-mode-tools-git.git
+git clone https://aur.archlinux.org/schedtoold-git.git
+git clone https://aur.archlinux.org/zoom.git
+git  clone https://aur.archlinux.org/ventoy-bin.git
+git clone https://aur.archlinux.org/visual-studio-code-bin.git
+git clone https://aur.archlinux.org/proton-ge-custom-bin.git
+git clone https://aur.archlinux.org/teams-for-linux-bin.git
+git clone https://aur.archlinux.org/sound-theme-smooth.git
 git clone https://github.com/Frogging-Family/nvidia-all.git
 git clone https://github.com/Frogging-Family/wine-tkg-git.git
+
 mkdir ./repo/
 cp -ra ./post.sh ./repo/post.sh
 chmod +x ./repo/post.sh
