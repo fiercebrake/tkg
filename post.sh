72915afe64eb1bbca94b701d9c0ecdd1e4a028df
David Valiente <fierce.brake@gmail.com>
2025-09-10 11:49:00 -0600

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/ntl
-sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/amd
+# sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/ntl
+# sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/amd
 sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/onedrive-abraunegg
 sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/google-chrome
 sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/microsoft-edge-stable-bin
@@ -19,6 +19,7 @@ sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --forc
 sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/proton-ge-custom-bin
 sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/teams-for-linux-bin
 sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/sound-theme-smooth
+sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/pwvucontrol
 # sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --dir /mnt/tkg/nvidia-all
 # sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --dir /mnt/tkg/wine-tkg-git/wine-tkg-git
 
@@ -41,8 +42,9 @@ mv /mnt/tkg/visual-studio-code-bin/*.pkg.tar.zst /mnt/tkg/repo
 mv /mnt/tkg/proton-ge-custom-bin/*.pkg.tar.zst /mnt/tkg/repo
 mv /mnt/tkg/teams-for-linux-bin/*.pkg.tar.zst /mnt/tkg/repo
 mv /mnt/tkg/sound-theme-smooth/*.pkg.tar.zst /mnt/tkg/repo
-# mv /mnt/tkg/nvidia-all/*.pkg.tar.zst /mnt/tkg/repo
-# mv /mnt/tkg/wine-tkg-git/wine-tkg-git/*.pkg.tar.zst /mnt/tkg/repo
+mv /mnt/tkg/pwvucontrol/*.pkg.tar.zst /mnt/tkg/repo
+mv /mnt/tkg/nvidia-all/*.pkg.tar.zst /mnt/tkg/repo
+mv /mnt/tkg/wine-tkg-git/wine-tkg-git/*.pkg.tar.zst /mnt/tkg/repo
 
 rm -rf /mnt/tkg/repo/themis*
 repo-add -n -v /mnt/tkg/repo/themis.db.tar.gz /mnt/tkg/repo/*.pkg.tar.zst
