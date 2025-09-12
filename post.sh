88852a84bfa3b57436cb7e4eb797143a35199a7c
David Valiente <fierce.brake@gmail.com>
2025-09-12 07:01:38 -0600

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-# sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/ntl
-# sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/amd
+sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/ntl
+sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/amd
 sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/onedrive-abraunegg
 sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/google-chrome
 sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/microsoft-edge-stable-bin
@@ -19,7 +19,7 @@ sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --forc
 sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/proton-ge-custom-bin
 sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/teams-for-linux-bin
 sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/sound-theme-smooth
-sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/pwvucontrol
+sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/networkmanager-ssh
 # sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --dir /mnt/tkg/nvidia-all
 # sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --dir /mnt/tkg/wine-tkg-git/wine-tkg-git
 
@@ -42,7 +42,7 @@ mv /mnt/tkg/visual-studio-code-bin/*.pkg.tar.zst /mnt/tkg/repo
 mv /mnt/tkg/proton-ge-custom-bin/*.pkg.tar.zst /mnt/tkg/repo
 mv /mnt/tkg/teams-for-linux-bin/*.pkg.tar.zst /mnt/tkg/repo
 mv /mnt/tkg/sound-theme-smooth/*.pkg.tar.zst /mnt/tkg/repo
-mv /mnt/tkg/pwvucontrol/*.pkg.tar.zst /mnt/tkg/repo
+mv /mnt/tkg/networkmanager-ssh/*.pkg.tar.zst /mnt/tkg/repo
 mv /mnt/tkg/nvidia-all/*.pkg.tar.zst /mnt/tkg/repo
 mv /mnt/tkg/wine-tkg-git/wine-tkg-git/*.pkg.tar.zst /mnt/tkg/repo
 
