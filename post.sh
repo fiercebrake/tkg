6a23f7cf6dc292bee342488ad38b7fcb8e896327
David Valiente <fierce.brake@gmail.com>
2025-10-02 06:23:42 -0600

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,19 +1,41 @@
 #/bin/bash
 
 declare -a arr_packages=('onedrive-abraunegg' 'google-chrome' 'microsoft-edge-stable-bin' 'blesh-git' 'ocs-url' 'aic94xx-firmware' \
-	                       'ast-firmware' 'wd719x-firmware' 'upd72020x-fw' 'laptop-mode-tools-git' 'schedtoold' 'zoom' 'ventoy-bin' \
-			                   'visual-studio-code-bin' 'proton-ge-custom-bin' 'teams-for-linux-bin' 'sound-theme-smooth' 'networkmanager-ssh' \
-			                   'bitwarden-bin' 'amd' 'ntl' 'wine-tkg-git' 'nvidia-all')
+                         'ast-firmware' 'wd719x-firmware' 'upd72020x-fw' 'laptop-mode-tools-git' 'schedtoold' 'zoom' 'ventoy-bin' \
+                         'visual-studio-code-bin' 'proton-ge-custom-bin' 'teams-for-linux-bin' 'sound-theme-smooth' \
+                         'networkmanager-ssh' 'bitwarden-bin' 'pikaur' 'yubico-authenticator-bin' 'bibata-cursor-theme-bin' \
+                         'flat-remix' 'kora-icon-theme' 'ttf-ms-win10-auto' 'heroic-games-launcher')
+
+
+function del_folder() {
+  rm -rf /mnt/tkg/$1
+}
+
+
+function get_folder() {
+  domain='aur.archlinux.org'
+  sudo -u repo git clone https://aur.archlinux.org/$1.git /mnt/tkg/$1
+}
+
+
+function get_package() {
+  sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/$1
+}
+
 
 for package in "${arr_packages[@]}";
 do
-  sudo -u repo makepkg --needed --noconfirm --syncdeps --cleanbuild --clean --force --dir /mnt/tkg/"$package"
+  del_folder $package
+
+  get_folder $package
+
+  get_package $package
 done
+
+sudo -u repo mkdir /mnt/tkg/repo
 
 mv --force /mnt/tkg/*/*.pkg.tar.zst /mnt/tkg/repo
 
 rm -rf /mnt/tkg/repo/themis*
+
 repo-add -n -v /mnt/tkg/repo/themis.db.tar.gz /mnt/tkg/repo/*.pkg.tar.zst
-# scp -P 45100 ./tekne* ./*.pkg.tar.zst devops@sivaractors.com:x86_64/
-# ssh -p 45100 devops@sivaractors.com "sudo chown -R devops:nginx /home/devops/x86_64/"
-# update
